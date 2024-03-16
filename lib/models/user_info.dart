d3643ae2f070d59d0c9b86bca8a40ead5fabe910
datdv <datbst1102@gmail.com>
2024-03-16 13:41:15 +0700

    dev2

--- lib/models/user_info.dart
+++ lib/models/user_info.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 
+import 'dart:convert';
+
 import 'package:flutter_app_bt/models/address_info.dart';
 
 class UserInfo {
@@ -17,13 +19,69 @@ class UserInfo {
   });
 
 
-  factory UserInfo.fromMap(Map<String, dynamic> map){
+  factory UserInfo.fromMap(Map<String, dynamic> map) {
     return UserInfo(
-        name: map['name'],
-        email: map['email'],
-        phoneNumber: map['phoneNumber'],
-        birthDate: map['birthDate'],
-        address: map['address'] ? AddressInfo.fromMap(map['address'] as Map<String, dynamic>) : null
-    );   
+      name: map['name'] != null ? map['name'] as String : null,
+      email: map['email'] != null ? map['email'] as String : null,
+      phoneNumber: map['phoneNumber'] != null ? map['phoneNumber'] as String : null,
+      birthDate: map['birthDate'] != null ? DateTime.fromMillisecondsSinceEpoch(map['birthDate'] as int) : null,
+      address: map['address'] != null ? AddressInfo.fromMap(map['address'] as Map<String,dynamic>) : null,
+    );
+  }
+
+  UserInfo copyWith({
+    String? name,
+    String? email,
+    String? phoneNumber,
+    DateTime? birthDate,
+    AddressInfo? address,
+  }) {
+    return UserInfo(
+      name: name ?? this.name,
+      email: email ?? this.email,
+      phoneNumber: phoneNumber ?? this.phoneNumber,
+      birthDate: birthDate ?? this.birthDate,
+      address: address ?? this.address,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'name': name,
+      'email': email,
+      'phoneNumber': phoneNumber,
+      'birthDate': birthDate?.millisecondsSinceEpoch,
+      'address': address?.toMap(),
+    };
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory UserInfo.fromJson(String source) => UserInfo.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'UserInfo(name: $name, email: $email, phoneNumber: $phoneNumber, birthDate: $birthDate, address: $address)';
+  }
+
+  @override
+  bool operator ==(covariant UserInfo other) {
+    if (identical(this, other)) return true;
+  
+    return 
+      other.name == name &&
+      other.email == email &&
+      other.phoneNumber == phoneNumber &&
+      other.birthDate == birthDate &&
+      other.address == address;
+  }
+
+  @override
+  int get hashCode {
+    return name.hashCode ^
+      email.hashCode ^
+      phoneNumber.hashCode ^
+      birthDate.hashCode ^
+      address.hashCode;
   }
 }
--- lib/views/profile_view.dart
+++ lib/views/profile_view.dart
@@ -44,11 +44,184 @@ class _ProfileViewState extends State<ProfileView> {
           });
         }
       }else if(currentStep == 1){
+        if (value > currentStep) {
+          if (step2FormKey.currentState!.validate()) {
+             step2FormKey.currentState!.save();
+            setState(() {
+              currentStep = value;
+            });
+          }
+        }else {
+           setState(() {
+              currentStep = value;
+            });
+        }
+      } else if(currentStep == 2) {
+        setState(() {
+          if (value < currentStep) {
+            currentStep = value;
+          } else {
+            saveUserInfo(userInfo).then((value) {
+              showDialog<void>(
+                context: context, 
+                barrierDismissible: false,
+                builder: (BuildContext context) {
+                  return AlertDialog(
+                      title: const Text("Thông báo"),
+                      content: const SingleChildScrollView(
+                        child: ListBody(
+                          children: [
+                            Text("Hồ sơ người dùng đã được lưu thành công"),
+                            Text("Bạn có thể quay lại các bước để cập nhật"),
+                          ],
+                        ),
+                      ),
+                      actions: [
+                        TextButton(
+                          child: const Text("Đóng"),
+                          onPressed: (){
+                            Navigator.of(context).pop();
+                          },
+                          )
+                      ],
+                  );
+                }
+              );
+            });
+          }
+        }); 
       }
     }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Cập nhật hồ sơ"),
+        actions: [
+          IconButton(
+            onPressed: (){
+            showDialog(
+              context: context, 
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                  return AlertDialog(
+                    title: const Text("Xác nhận"),
+                    content: const Text("Bạn có muốn xóa thông tin đã lưu?"),
+                    actions: <Widget>[
+                      TextButton(
+                        onPressed: (){
+                          Navigator.of(context).pop(false);
+                        }, 
+                      child: const Text('Hủy')
+                      ),
+                      TextButton(
+                        onPressed: (){
+                          Navigator.of(context).pop(true);
+                        }, 
+                      child: const Text('Đồng ý')
+                      ),
+                    ],
+                  );
+              }
+            ).then((value) {
+              if (value != null && value == true) {
+                setState(() {
+                  userInfo = UserInfo();
+                });
+                saveUserInfo(userInfo);
+              }
+            });
+          }, 
+          icon: const Icon(Icons.delete_outline))
+        ],
+      ),
+      body: FutureBuilder<UserInfo>(
+        future: init(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            userInfo = snapshot.data!;
+            return Stepper(
+              type: StepperType.horizontal,
+              currentStep: currentStep,
+              controlsBuilder: (context, details) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Wrap(
+                      children: [
+                        if(currentStep == 2)
+                          FilledButton(onPressed: details.onStepContinue, child: const Text("Lưu"))
+                        else 
+                          FilledButton(onPressed: details.onStepContinue, child: const Text("Tiếp")),
+                        if(currentStep > 0)
+                          TextButton(onPressed: details.onStepCancel, child: const Text("Quay lại")),
+                        if(currentStep == 2) 
+                          OutlinedButton(onPressed: (){}, child: const Text("Đóng"))
+                      ],
+                    )
+                  ],
+                );
+              },
+              onStepTapped: (value) {
+                updateStep(value);
+              },
+              onStepContinue: () {
+                updateStep(currentStep + 1);
+              },
+              onStepCancel: () {
+                if (currentStep > 0) {
+                  setState(() {
+                    currentStep--;
+                  });
+                }
+              },
+              steps: [Step(title: const Text("Cơ bản"), content: Step1Form())],
+            );
+          } else {
+            return const Center(child: LinearProgressIndicator(),);
+          }
+        },
+      ),
+    );
   }
 }
 
+
+class Step1Form extends StatefulWidget {
+  const Step1Form({super.key});
+
+  @override
+  State<Step1Form> createState() => _Step1FormState();
+}
+
+class _Step1FormState extends State<Step1Form> {
+  final nameCtl = TextEditingController();
+  final dateCtl = TextEditingController();
+  final emailCtl = TextEditingController();
+  final phoneCtl = TextEditingController();
+
+  bool isEmailValid(String email) {
+    String pattern = r"";
+
+    final emailRegex = RegExp(pattern);
+    return emailRegex.hasMatch(email);
+  }
+
+    bool isMobileValid(String email) {
+    String pattern = r"";
+
+    final emailRegex = RegExp(pattern);
+    return emailRegex.hasMatch(email);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+Future<void> saveUserInfo(UserInfo userInfo) async {
+  return await Localstore.instance.collection('users').doc('info').set(userInfo.toMap());
+}
 
 Future<Map<String, dynamic>?> loadUserInfo() async{
   return await Localstore.instance.collection('users').doc('info').get();
